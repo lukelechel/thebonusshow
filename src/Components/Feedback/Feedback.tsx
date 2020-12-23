d98810c8da25bb96bcd17a45f261ee642bd872d0
Luke Lechel <13129882+lukelechel@users.noreply.github.com>
2020-12-23 00:27:55 -0500

    Added feedback form

--- src/Components/Feedback/Feedback.tsx
+++ src/Components/Feedback/Feedback.tsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Feedback.css'
 
 import Nav from '../Nav/Nav'
 
+function FeedbackForm() {
+    // state management
+    const [name, setName] = useState(String)
+    const [email, setEmail] = useState(String)
+    const [message, setMessage] = useState(String)
+    const [loading, setLoading] = useState(false)
+    const [formSubmitted, setformSubmitted] = useState(false)
+    const combinedState = {name: name, email: email, message: message}
+
+    function handleSubmission(e:any) {
+        e.preventDefault()
+        setLoading(true)
+        
+        const url = "https://ljzvvaf4r4.execute-api.us-east-2.amazonaws.com/Prod/submitForm" // Endpoint + /submitForm
+        fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'applications/json',
+                    'Accept': 'applications/json'
+                },
+                body: JSON.stringify(combinedState),
+            })
+            .then(response => response.text())
+            .then(text => {
+                console.log(text)
+                console.log('success')
+                formReset()
+            })
+            .catch((error) => {
+                console.error(error)
+            }
+        )
+    }
+
+    function formReset() {
+        setName("")
+        setEmail("")
+
+        // Clear inputs from form
+        Array.from(document.querySelectorAll('input')).forEach(input => input.value='')
+        Array.from(document.querySelectorAll('textarea')).forEach(input => input.value='')
+
+        setLoading(false)
+        setformSubmitted(true)
+    }
+
+    const returnSpinner = () => (<div id="spinner">
+        <h3>Sending message...</h3>
+        {/* ideally add spinner here */}
+    </div>)
+
+    const returnDone = () => (<div id="formDone">
+        <img src="https://thebonusshow-resources.s3.us-east-2.amazonaws.com/checkmark.svg" id="doneCheckmark" alt="Checkmark" />
+        <h1>Successfully sent.</h1>
+    </div>)
+
+    return (<form id="feedback-form">
+        <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            className="formInput"
+            onChange={(e) => setName(e.target.value)}
+            required />
+        <input type="text"
+            placeholder="Email address"
+            name="email"
+            className="formInput"
+            onChange={(e) => setEmail(e.target.value)}
+            required />
+        <textarea
+            placeholder="Message"
+            name="message"
+            className="formInput"
+            onChange={(e) => setMessage(e.target.value)}
+            maxLength={3000}
+            required />
+        {
+            loading ? returnSpinner() : <input type="button" value="Submit" id="submitButton" onClick={handleSubmission} />
+        }
+        {
+            formSubmitted ? returnDone() : null
+        }
+</form>)
+}
+
 const Feedback = () => (<div>
     <Nav />
     <h1>Feedback</h1>
+    <FeedbackForm />
 </div>)
 
 export default Feedback
