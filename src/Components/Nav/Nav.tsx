06195a06875aea7bd1311057cd8edb3053722876
Luke Lechel <13129882+lukelechel@users.noreply.github.com>
2021-01-23 22:01:33 -0500

    Better fleshed out Nav

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.css'
 import { Link } from 'react-router-dom'
 
-const navigationLibrary = [
+const navLibrary = [
     {
         title: "Home",
         link: "/"
@@ -17,16 +17,44 @@ const navigationLibrary = [
     }
 ]
 
-const Nav = () => (<div id="nav-container">
-    <ul id="nav-items">
-        {navigationLibrary.map(x => (
-            <li key={x.title}>
-                <Link to={x.link}>
-                    {x.title}
-                </Link>
-            </li>
-        ))}
-    </ul>
-</div>)
+export default function Nav() {
+    const [menuOpen, setMenuOpen] = useState(false)
 
-export default Nav
+    function toggleMenu() {
+        menuOpen ? setMenuOpen(false) : setMenuOpen(true)
+    }
+
+    return (<div id="nav-container">
+        <div id="desktop-nav" className="desktopOnly">
+            <ul>
+                {navLibrary.map(x => (
+                    <li key={x.title}>
+                        <Link to={x.link}>
+                           {x.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <div id="mobile-nav" className="mobileOnly">
+            <svg viewBox="0 0 100 75" width="35" height="35"
+                id="menuIcon"
+                onClick={toggleMenu}>
+                <rect width="100" height="11" rx="4"></rect>
+                <rect y="30" width="100" height="11" rx="4"></rect>
+                <rect y="60" width="100" height="11" rx="4"></rect>
+            </svg>
+            <div className={menuOpen === true ? 'visible' : 'invisible'}>
+                <ul>
+                    {navLibrary.map(x => (
+                        <li key={x.title}>
+                            <Link to={x.link}>
+                                {x.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    </div>)
+}
